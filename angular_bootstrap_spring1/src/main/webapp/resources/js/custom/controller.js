2d72ae1b56d7deb30b55a4b4af83597a3d36fceb
Dariusz Kobuszewski <dariusz.kobuszewski@doskomp.lodz.pl>
2015-01-06 03:11:29 +0100

    Edycja użytkowników, powinna dzialać.

--- angular_bootstrap_spring1/src/main/webapp/resources/js/custom/controller.js
+++ angular_bootstrap_spring1/src/main/webapp/resources/js/custom/controller.js
@@ -13,8 +13,17 @@ app.controller('MainController', function ($rootScope, $scope, $location) {
 });
 
 app.controller('UsersController', function ($rootScope, $scope, $http, CustomerService) {
+    // Initially get users list:
     $http.get('/rest/users')
         .then(function(response){
             $scope.users = response.data;
         });
+
+    $scope.saveUser = function(user) {
+        $http.post('/rest/save', user)
+            .then(function(response) {
+                console.log("User saved, response: ");
+                console.log(response);
+            });
+    }
 });
